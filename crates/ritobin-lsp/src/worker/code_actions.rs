04a0058c0c4191478798f84e21a78c6ca12e6d55
Alan Panayotov <alanpanayotov@gmail.com>
2026-08-19 19:07:06 +0000

    feat: "remove unknown" action + lint/action for mismatched meta prop types

--- crates/ritobin-lsp/src/worker/code_actions.rs
+++ crates/ritobin-lsp/src/worker/code_actions.rs
@@ -3,11 +3,14 @@ use std::collections::HashMap;
 use lsp_types::{
     CodeAction, CodeActionOrCommand, CodeActionResponse, Diagnostic, Range, TextEdit, WorkspaceEdit,
 };
-use ritobin_lsp::cst_ext::CstExt;
+use ltk_ritobin::{RitoType, cst::NodeId, parse::Span};
 
 use crate::worker::Worker;
 
-pub enum CodeActionData {}
+pub enum CodeActionData {
+    TypeMismatch { type_expr: Span, expected: RitoType },
+    RemoveEntry(NodeId),
+}
 
 impl Worker {
     pub(crate) fn register_code_action(&mut self, data: CodeActionData) -> u32 {
@@ -37,7 +40,48 @@ impl Worker {
         let idx = diagnostic.data.and_then(|d| d.as_u64())?;
         let data = self.code_action_data.get(idx as usize)?;
         Some(match data {
-            _ => return None,
+            CodeActionData::TypeMismatch {
+                type_expr,
+                expected,
+            } => {
+                let new_text = expected.to_string();
+                CodeActionOrCommand::CodeAction(CodeAction {
+                    title: format!("Change this entry's type to {new_text}",),
+                    edit: Some(WorkspaceEdit {
+                        changes: Some(HashMap::from_iter([(
+                            self.document.uri.clone(),
+                            [TextEdit {
+                                range: self.document.line_numbers.from_span(*type_expr),
+                                new_text,
+                            }]
+                            .into(),
+                        )])),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+            }
+            CodeActionData::RemoveEntry(entry) => {
+                let cst = self.cst.as_ref()?;
+
+                let node = cst.node(*entry)?;
+
+                CodeActionOrCommand::CodeAction(CodeAction {
+                    title: "Remove this entry".into(),
+                    edit: Some(WorkspaceEdit {
+                        changes: Some(HashMap::from_iter([(
+                            self.document.uri.clone(),
+                            [TextEdit {
+                                range: self.document.line_numbers.from_span(node.span),
+                                new_text: String::new(),
+                            }]
+                            .into(),
+                        )])),
+                        ..Default::default()
+                    }),
+                    ..Default::default()
+                })
+            }
         })
     }
 }
